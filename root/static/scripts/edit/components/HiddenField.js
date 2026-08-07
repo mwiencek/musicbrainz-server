0e15193f124f70e6a865ccf1d602ccc8433f47af
Michael Wiencek <mwtuea@gmail.com>
2026-08-06 21:30:27 -0500

    Add a `HiddenFields` component which accepts `AnyFieldT`
    Allows rendering all hidden subfields of a compound/repeatable field.
    
    `serializeFieldValue` is needed to refine the `unknown` part of
    `FieldT<unknown>`.

--- root/static/scripts/edit/components/HiddenField.js
+++ root/static/scripts/edit/components/HiddenField.js
@@ -7,14 +7,50 @@
  * later version: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-component HiddenField(field: FieldT<number | string>, value?: string) {
+import * as React from 'react';
+
+function serializeFieldValue(value: unknown): string {
+  if (value == null) {
+    return '';
+  }
+  if (typeof value === 'boolean') {
+    return value ? '1' : '0';
+  }
+  return String(value);
+}
+
+component HiddenField(field: FieldT<unknown>, value?: string) {
   return (
     <input
       name={field.html_name}
       type="hidden"
-      value={value ?? field.value}
+      value={value ?? serializeFieldValue(field.value)}
     />
   );
 }
 
+component _HiddenFields(field: AnyFieldT) {
+  return match (field) {
+    {type: 'compound_field', ...} as compoundField => (
+      Object.values(compoundField.field).map((subfield) => (
+        <HiddenFields field={subfield} key={subfield.html_name} />
+      ))
+    ),
+    {type: 'field', ...} as scalarField => (
+      <HiddenField field={scalarField} />
+    ),
+    {type: 'repeatable_field', ...} as repeatableField => (
+      repeatableField.field.map((subfield) => (
+        subfield == null
+          ? null
+          : <HiddenFields field={subfield} key={subfield.html_name} />
+      ))
+    )
+  };
+}
+
+const HiddenFields: component(field: AnyFieldT) = React.memo(_HiddenFields);
+
 export default HiddenField;
+
+export {HiddenFields};
